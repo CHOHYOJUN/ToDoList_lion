03099c221045cb9d1f0902f5bf5ea08324483d6b
lionjo <lionjerher84@gmail.com>
2018-11-27 01:21:16 +0900

    프론트 - vue 페이지, 라우터 리펙토링

--- be/fe/src/App.vue
+++ be/fe/src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <router-view/>
-    </v-content>
+    여기서부터 코드 시작
+    <!-- 라우팅에 따라 컴포넌트가 대체 되는 부분 -->
+      <router-view></router-view> 
   </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  name: 'app',
   data () {
     return {
       //
