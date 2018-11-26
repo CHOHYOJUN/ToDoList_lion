9e05695a779151e88aa107cac78b060fe57ff955
lionjo <lionjerher84@gmail.com>
2018-11-27 01:45:05 +0900

    board 컴퍼넌트 추가, 동적 라우트 매칭

--- be/fe/src/App.vue
+++ be/fe/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <v-app>
-    여기서부터 코드 시작
+  <div id="app">
+    <Navbar />
     <!-- 라우팅에 따라 컴포넌트가 대체 되는 부분 -->
       <router-view></router-view> 
-  </v-app>
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 
 export default {
   name: 'app',
+  components : {
+      Navbar
+  },
   data () {
     return {
       //
--- be/fe/src/router/index.js
+++ be/fe/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
 import NotFound from '../views/NotFound.vue'
+
 import Login from '../components/Login.vue'
+import Board from '../components/Board.vue'
 
 Vue.use(Router)
 
@@ -19,6 +21,10 @@ export default new Router({
       path: '/login',
       // name: 'login',
       component: Login
+    },
+    {
+      path: '/b/:bid', // : 콜론이 있을경우 뒤의 bid 변수로 받을 수 있음
+      component: Board
     },
     {
       path: '*',
